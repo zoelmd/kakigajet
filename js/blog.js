23221a283ee3de251c2eb383f2e1cd547e4c4547
zoelmd <zulwadi_mw@yahoo.com>
2015-11-12 23:27:28 +0800

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,6 @@
 $(function() {
  
-    Parse.$ = jQuery;
+
  
     // Replace this line with the one on your Quickstart Guide Page
     Parse.initialize("nQkZtiTz13AY5YBulsOWNdmupWpl1JLrnykc4dYW", "jRGFMGaqXwvo1rHBxRAQARDVgJTDU0RCkd2u7XtS");
