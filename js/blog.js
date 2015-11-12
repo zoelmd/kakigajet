c9565f6cbec4142759972eeb9fbf8e60b53438b5
zoelmd <zulwadi_mw@yahoo.com>
2015-11-12 19:45:16 +0800

    add blog.js

---
+++ js/blog.js
@@ -0,0 +1,25 @@
+$(function() {
+ 
+    Parse.$ = jQuery;
+ 
+    // Replace this line with the one on your Quickstart Guide Page
+    Parse.initialize("nQkZtiTz13AY5YBulsOWNdmupWpl1JLrnykc4dYW", "jRGFMGaqXwvo1rHBxRAQARDVgJTDU0RCkd2u7XtS");
+
+ 
+   var Blog = Parse.Object.extend("Blog");
+    var Blogs = Parse.Collection.extend({
+    model: Blog
+});
+
+var blogs = new Blogs();
+blogs.fetch({
+    success: function(blogs) {
+        console.log(blogs);
+    },
+    error: function(blogs, error) {
+        console.log(error);
+    }
+});
+
+ 
+});
