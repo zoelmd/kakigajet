e121c627309fc642dbc734d251f3e0ad24839c6c
zoelmd <zulwadi_mw@yahoo.com>
2015-11-13 00:31:07 +0800

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,11 +1,14 @@
-
+$(function() {
+ 
+    Parse.$ = jQuery;
+    
     // Replace this line with the one on your Quickstart Guide Page
     Parse.initialize("nQkZtiTz13AY5YBulsOWNdmupWpl1JLrnykc4dYW", "jRGFMGaqXwvo1rHBxRAQARDVgJTDU0RCkd2u7XtS");
 
  
    var Blog = Parse.Object.extend("Blog");
-   var Blogs = Parse.Object.extend({
-       model: "Blog"});
+   var blogs = Parse.Object.extend({
+       model: kakigajet});
 
 var blogs = new Blogs();
 blogs.fetch({
@@ -17,3 +20,4 @@ blogs.fetch({
     }
 });
 
+});
